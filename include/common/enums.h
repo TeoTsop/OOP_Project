8276b25875cc44006c1d1ba9c34882bf01d7152f
TeoTsop <teo.tsopelogiannis@gmail.com>
2026-01-08 22:22:04 +0200

    Enums

--- include/common/enums.h
+++ include/common/enums.h
@@ -1,3 +1,29 @@
 //All of the enums
 
 #pragma once
+
+//The four cardinal directions
+enum Direction {SOUTH,
+                NORTH,
+                EAST,
+                WEST};
+
+//The speed states an object can be
+enum SpeedState {STOPPED,
+                 HALF_SPEED,
+                 FULL_SPEED
+                };
+
+//All possible objects in the grid
+enum ObjectType {AUTONOMUS_VEHICLE,
+                 MOVING_VEHICLE,
+                 STOPPED_VEHICLE,
+                 MOVING_BIKE,
+                 TRAFFIC_SIGN,
+                 TRAFFIC_LIGHT
+                };
+
+//All traffic colors
+enum TrafficLightColor {RED,
+                        YELLOW,
+                        GREEN};
